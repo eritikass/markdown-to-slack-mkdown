bf56ca9706f9df79a701826d77e98c8aadf73021
Siim Tiilen <eritikass@gmail.com>
2025-08-11 22:09:40 +0300

    cleanup

--- tmp/test_quick.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Quick one-liner test with Python 3.11
-docker run --rm python:3.11-slim bash -c 'pip install -q --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple/ markdown-to-slack-mkdown && python -c "from markdown_to_slack_mkdown import slack_convert; print(slack_convert(\"**Hello** [World](https://example.com) #123\"))"'
